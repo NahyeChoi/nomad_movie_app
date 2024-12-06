c73b5218c163d4a64d073c62ebf7b91fc27d2fb5
Nahye Choi <skgp0628@naver.com>
2024-12-06 23:54:03 +0900

    Today I Learned #3.0 Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,45 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Musical ({ name, image, rating }){ 
-  return (
-    <div>
-      <h2>뮤지컬 {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={image} alt={name} />
-    </div>
-  )
-}
-
-Musical.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
-
-const musicalList = [
-  {
-    id: 1,
-    name: "키다리 아저씨",
-    image: "http://tkfile.yes24.com/upload2/PerfBlog/202310/20231011/20231011-47438.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "이터니티",
-    image: "http://tkfile.yes24.com/upload2/PerfBlog/202411/20241118/20241118-50610.jpg",
-    rating: 4.9
+class App extends React.Component{
+  state = {
+    count: 0
   }
-]
-
-function App() {
-  return (
-    <div>
-      {musicalList.map(musical => (
-        <Musical key={musical.id} name={musical.name} image={musical.image} rating={musical.rating} />
-      ))}
-    </div>
-  );
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState({ count: this.state.count - 1});
+  };
+  render(){
+    return (
+      <div>
+        <h1> The number is {this.state.count} </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
