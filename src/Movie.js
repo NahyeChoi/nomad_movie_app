5260bd47791ecf231e2b6d42a5568bc4e750cce9
Nahye Choi <skgp0628@naver.com>
2024-12-16 23:57:50 +0900

    Today I Learned #4.2 Styling the Movies #4.3 Adding Genres #4.4 Styles Timelapse

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}){
-    return <h4>{title}</h4>
+function Movie({year, title, summary, poster, genres}){
+    return (
+        <div className="movies">
+            <img src={poster} alt={title} title={title}/>
+
+            <div className="movie_data">
+                <h3 className="movie_title">{title}</h3>
+                <h3 className="movie_year">{year}</h3>
+                <ul className="genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres_genre">{genre}</li>
+                    ))}
+                </ul>
+                <p className="movie_summary">{summary.slice(0, 180)}</p>
+            </div>
+            
+        </div>
+    )
+    
 }
 
 Movie.prototype = {
@@ -10,7 +28,8 @@ Movie.prototype = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
